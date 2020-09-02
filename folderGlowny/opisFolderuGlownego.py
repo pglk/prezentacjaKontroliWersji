5ee70a9ff5138d96764a1e2f0c732e65f3a19424
Paweł <pgolkowski@jm.pl>
2020-09-02 10:19:41 +0200

    Uzupełniłem opis folderu głównego
    Ma to na celu zaprezentowanie możliwości tworzenia branchy.

--- folderGlowny/opisFolderuGlownego.py
+++ folderGlowny/opisFolderuGlownego.py
@@ -1,1 +1,3 @@
+#To jest folder główny programu
+
 #W celu strukturyzacji sugeruję, aby w każdym projekcie tworzyć folder główny, w którym zebrane będą skrypty, pakiety, oraz moduły danego programu.
